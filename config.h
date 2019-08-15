8d86d81803f1a5c6d5131743ef551dcba7b33f99
Kyoz <banminkyoz@gmail.com>
2019-08-15 18:36:53 +0700

    update font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Term:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka Term:pixelsize=24:antialias=true:autohint=true";
 
 /* options to disable bold, italic and roman fonts globally */
 int disablebold = 1;
