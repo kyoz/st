04d67122d3f9627e4b196e1a5b6188860f74614a
Kyoz <banminkyoz@gmail.com>
2020-09-10 21:56:25 +0700

    add(patch): copyurl-0.8.1

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka Nerd Font:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -200,6 +200,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+  { ShiftMask,            XK_L,           copyurl,        {.i =  0} },
 };
 
 /*
